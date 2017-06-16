cc66c334f9b9d28417661bc73f758dc577ec7d2c
maybe <985962824@qq.com>
2017-06-16 16:03:58 +0800

    new update A.java
    在A.java中实现了add方法

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -2,5 +2,10 @@ public class A {
     public static void main (String[] args) {
         System.out.println("A.java");
     }
+    
+    //new增加的方法
+    public static void add () {
+        System.out.println("add");
+    }
 }
 
